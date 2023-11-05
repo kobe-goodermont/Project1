e563f0eda7b4ecd6191f5eac751d0c5818a0e471
kgooderm@uccs.edu <kgooderm@uccs.edu>
2023-11-04 23:35:17 -0600

    Commit all code

---
+++ Project1_app/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from Project1_app.models import Workout
+
+# Register your models here.
+admin.site.register(Workout)
---
+++ Project1_app/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+# Create your models here.
+
+
+class Workout(models.Model):
+
+    TITLE = models.CharField(max_length=200)
+    DESCRIPTION = models.CharField(max_length=200)
+    CHOICES = (
+        ('Chest', 'Chest'),
+        ('Arms', 'Arms'),
+        ('Legs', 'Legs'),
+        ('Back', 'Back'),
+        ('Shoulders', 'Shoulders'),
+
+    )
+    GROUP = models.CharField(max_length=200, choices=CHOICES)
